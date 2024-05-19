1dd95f1299cbc90a79b3f6dcdd2b3d96cbaedafb
Jared Muñoz <amunozbaez669@gmail.com>
2024-05-18 19:44:49 -0600

    chore: Add carousel component for recent complaints section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,14 @@ import { auth } from "@/auth";
 import Complaint from "@/components/complaint";
 import ComplaintFilter from "@/components/complaint-filter";
 import { Button, buttonVariants } from "@/components/ui/button";
+import { Card, CardBody } from "@/components/ui/card";
+import {
+	Carousel,
+	CarouselContent,
+	CarouselItem,
+	CarouselNext,
+	CarouselPrevious,
+} from "@/components/ui/carousel";
 import {
 	DropdownMenu,
 	DropdownMenuContent,
@@ -13,6 +21,7 @@ import {
 	DropdownMenuSeparator,
 	DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import Autoplay from "embla-carousel-autoplay";
 import { MoreVertical } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
@@ -33,62 +42,80 @@ export default async function Home({
 	);
 
 	return (
-		<main className="max-w-6xl mx-auto py-8">
-			<header className="flex flex-row justify-between items-center mb-4">
-				<h2 className="text-2xl font-medium">Quejas recientes</h2>
-				<DropdownMenu>
-					<DropdownMenuTrigger asChild className="inline-block md:hidden">
-						<Button variant="outline" size="icon">
-							<MoreVertical />
-						</Button>
-					</DropdownMenuTrigger>
-					<DropdownMenuContent>
-						<DropdownMenuItem>
-							<ComplaintFilter />
-						</DropdownMenuItem>
-						<DropdownMenuSeparator />
-						<DropdownMenuItem>
-							<Link href="/complaints" className="w-full h-full">
-								Ver todas
-							</Link>
-						</DropdownMenuItem>
-						{session && (
+		<main className="max-w-6xl mx-auto py-8 flex flex-col gap-y-8">
+			<Carousel>
+				<CarouselContent>
+					{[1, 2, 3, 4, 5, 6].map((i) => (
+						<CarouselItem key={i} className="md:basis-1/2 lg:basis-1/2">
+							<div className="p-1">
+								<Card>
+									<CardBody className="flex aspect-video items-center justify-center p-6">
+										<span className="text-3xl font-semibold">{i}</span>
+									</CardBody>
+								</Card>
+							</div>
+						</CarouselItem>
+					))}
+				</CarouselContent>
+				<CarouselPrevious />
+				<CarouselNext />
+			</Carousel>
+			<section>
+				<header className="flex flex-row justify-between items-center mb-4">
+					<h2 className="text-2xl font-medium">Quejas recientes</h2>
+					<DropdownMenu>
+						<DropdownMenuTrigger asChild className="inline-block md:hidden">
+							<Button variant="outline" size="icon">
+								<MoreVertical />
+							</Button>
+						</DropdownMenuTrigger>
+						<DropdownMenuContent>
 							<DropdownMenuItem>
-								<Link href="/complaints/upload" className="w-full h-full">
+								<ComplaintFilter />
+							</DropdownMenuItem>
+							<DropdownMenuSeparator />
+							<DropdownMenuItem>
+								<Link href="/complaints" className="w-full h-full">
+									Ver todas
+								</Link>
+							</DropdownMenuItem>
+							<DropdownMenuItem>
+								<Link
+									href={session ? "/complaints/upload" : "/login"}
+									className="w-full h-full"
+								>
 									Hacer una queja
 								</Link>
 							</DropdownMenuItem>
-						)}
-					</DropdownMenuContent>
-				</DropdownMenu>
-				<div className="hidden md:flex flex-row gap-x-4 items-end">
-					<Link
-						href="/complaints"
-						className={buttonVariants({
-							variant: "link",
-						})}
-					>
-						{" "}
-						Ver todas{" "}
-					</Link>
-					<ComplaintFilter />
-					{session && (
+						</DropdownMenuContent>
+					</DropdownMenu>
+					<div className="hidden md:flex flex-row gap-x-4 items-end">
 						<Link
-							href="/complaints/upload"
+							href="/complaints"
+							className={buttonVariants({
+								variant: "link",
+							})}
+						>
+							{" "}
+							Ver todas{" "}
+						</Link>
+						<ComplaintFilter />
+						<Link
+							href={session ? "/complaints/upload" : "/login"}
 							className={buttonVariants({
 								variant: "default",
 							})}
 						>
 							Hacer una queja
 						</Link>
-					)}
+					</div>
+				</header>
+				<div className="flex flex-col gap-y-4">
+					{complaints.map((complaint) => (
+						<Complaint complaint={complaint} key={complaint._id} />
+					))}
 				</div>
-			</header>
-			<div className="flex flex-col gap-y-4">
-				{complaints.map((complaint) => (
-					<Complaint complaint={complaint} key={complaint._id} />
-				))}
-			</div>
+			</section>
 		</main>
 	);
 }
